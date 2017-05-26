19bf51610d197d938a04e8a54ebc9074f7bcae6a
Chad Phillips <chadphillips807@hotmail.com>
2017-05-26 11:29:11 -0700

    Created AppUser Model

---
+++ LiftRecorder/Models/AppUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LiftRecorder.Models
+{
+    public class AppUser
+    {
+        public int AppUserId { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // May need this Navigation Property
+        // This should link AppUser to the lifts they have performed
+        //public ICollection<UsersLift> UsersLifts { get; set; }
+
+    }
+}
